d143263a65671422eac37c2c48f30e6968170962
Will Dickson <wbd@localhost.localdomain>
2020-03-24 16:38:50 -0700

    Added gcode for laser cutting.

--- lasercut/laser_cut.py
+++ lasercut/laser_cut.py
@@ -5,6 +5,36 @@ from py2gcode import cnc_laser
 
 dxfFileName = sys.argv[1]
 
+layerList = ['vector{}'.format(i+1) for i in range(4)]
+
+for layer in layerList:
+    prog = gcode_cmd.GCodeProg()
+    prog.add(gcode_cmd.GenericStart())
+    prog.add(gcode_cmd.Space())
+    
+    param = {
+            'fileName'    :  dxfFileName,
+            'layers'      :  [layer,],
+            'dxfTypes'    :  ['LINE','ARC'],
+            'laserPower'  :  350,
+            'feedRate'    :  60,
+            'convertArcs' :  True,
+            'startCond'   : 'minX',
+            'direction'   : 'ccw',
+            'ptEquivTol'  :  0.4e-3,
+            }
+    
+    vectorCut = cnc_laser.VectorCut(param)
+    prog.add(vectorCut)
+    
+    prog.add(gcode_cmd.Space())
+    prog.add(gcode_cmd.End(),comment=True)
+    
+    baseName, ext = os.path.splitext(dxfFileName)
+    ngcFileName = '{0}_{1}.ngc'.format(baseName,layer)
+    prog.write(ngcFileName)
+
+
 prog = gcode_cmd.GCodeProg()
 prog.add(gcode_cmd.GenericStart())
 prog.add(gcode_cmd.Space())
@@ -12,7 +42,7 @@ prog.add(gcode_cmd.Space())
 
 param = {
         'fileName'    :  dxfFileName,
-        'layers'      :  ['vector'],
+        'layers'      :  layerList,
         'dxfTypes'    :  ['LINE','ARC'],
         'laserPower'  :  350,
         'feedRate'    :  60,
